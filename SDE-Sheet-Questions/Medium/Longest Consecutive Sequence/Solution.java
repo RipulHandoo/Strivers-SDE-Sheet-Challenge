30fd1b04d6ce7cb900333a670b64b9e3b0a9d527
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-06-30 23:44:01 +0530

    Create Solution.java

---
+++ SDE-Sheet-Questions/Medium/Longest Consecutive Sequence/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.* ;
+
+public class Solution {
+    public static int lengthOfLongestConsecutiveSequence(int[] nums, int N) {
+        // Write your code here.
+        Set < Integer > hashSet = new HashSet < Integer > ();
+        for (int num: nums) {
+            hashSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num: nums) {
+            if (!hashSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (hashSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
